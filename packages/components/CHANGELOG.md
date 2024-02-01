e2e0dc6755fc49f96b226ad9aa7b0abec0aa2438
Thinh Trinhduc <thinh.duc.trinh@gmail.com>
2024-02-01 20:54:26 +0700

    feat(usetoggle): added useToggle

--- packages/components/CHANGELOG.md
+++ packages/components/CHANGELOG.md
@@ -1,5 +1,12 @@
 # @react-awesome/components
 
+## 1.0.8
+
+### Patch Changes
+
+- Updated dependencies
+  - @react-awesome/use-toggle@0.0.1
+
 ## 1.0.7
 
 ### Patch Changes
---
+++ packages/use-toggle/src/useToggle/useToggle.tsx
@@ -0,0 +1,16 @@
+import React, { useCallback, useState } from 'react'
+
+export const useToggle = (initialValue: boolean) => {
+  const [value, setValue] = useState(initialValue)
+
+  const toggle = useCallback(
+    (_value?: boolean) =>
+      setValue((prev) => (typeof _value === 'boolean' ? _value : !prev)),
+    [],
+  )
+
+  return {
+    toggle,
+    isOn: value,
+  }
+}
