4dc1bad203e383fb4764be8f1862ee838d9233c4
Thinh Trinhduc <thinh.duc.trinh@gmail.com>
2024-02-06 00:39:19 +0700

    chore: working on usebreakpoint

--- apps/docs/src/components/Container.tsx
+++ apps/docs/src/components/Container.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { useEffect, useRef, useState } from 'react'
+import { Resizable } from 're-resizable'
 
 export const Container = ({
   children,
@@ -8,49 +8,36 @@ export const Container = ({
   children: React.ReactNode
   showSizeHandler?: boolean
 }) => {
-  const ref = useRef<HTMLDivElement>(null)
-  const [width, setWith] = useState<number | undefined>()
-
-  useEffect(() => {
-    if (!showSizeHandler || !ref.current) return
-
-    setWith(ref.current.clientWidth)
-
-    const callback = (entries: ResizeObserverEntry[]) => {
-      if (!entries.length) return
-
-      const [entry] = entries
-
-      const { target } = entry
-    }
-
-    const observer = new ResizeObserver(callback)
-
-    observer.observe(ref.current)
-
-    return () => {
-      observer.disconnect()
-    }
-  }, [showSizeHandler])
-
-  return (
-    <div
-      ref={ref}
-      className={classNames('mt-5 border rounded-lg p-4', {
-        'mr-9 relative': showSizeHandler,
-      })}
-      style={{
-        width,
-      }}
-    >
-      {children}
-
-      <div
-        className="pointer-events-auto absolute top-1/2 right-0 -mt-6 p-2 hidden md:block cursor-ew-resize select-none touch-pan-y origin-[50%_50%_0px] [transform:_translate3d(20px,_0px,_0px)_scale(1,_1)]"
-        draggable="false"
+  if (showSizeHandler) {
+    return (
+      <Resizable
+        bounds="parent"
+        enable={{
+          right: true,
+        }}
+        maxWidth="98%"
+        minWidth="30%"
+        handleComponent={{
+          right: (
+            <div
+              className="pointer-events-auto absolute top-1/2 -translate-y-1/2 hidden ml-3 md:block cursor-ew-resize"
+              draggable={false}
+              style={{
+                userSelect: 'none',
+                touchAction: 'pan-y',
+              }}
+            >
+              <div className="w-1.5 h-8 bg-slate-500/60 rounded-full"></div>
+            </div>
+          ),
+        }}
       >
-        <div className="w-1.5 h-8 bg-slate-500/60 rounded-full"></div>
-      </div>
-    </div>
-  )
+        <div className={classNames('mt-5 border rounded-lg p-4')}>
+          {children}
+        </div>
+      </Resizable>
+    )
+  }
+
+  return <div className="mt-5 border rounded-lg p-4">{children}</div>
 }
--- packages/use-breakpoint/src/useBreakpoint/useBreakpoint.tsx
+++ packages/use-breakpoint/src/useBreakpoint/useBreakpoint.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import _debounce from 'lodash/debounce'
 
 const DEFAULT_BREAKPOINTS = {
@@ -44,21 +44,53 @@ export function useBreakpoint<B extends Record<string, number>>(
     keyof typeof BPS | undefined
   >(fallbackValue as keyof typeof BPS)
 
+  const determineCurrentBreakpoint = useCallback(
+    ({ width }: { width: number; height: number }) => {
+      const sortedBreakpoints = Object.values(BPS).sort((a, b) => a - b)
+      for (let i = 0; i < sortedBreakpoints.length; i++) {
+        if (i === 0 && width <= sortedBreakpoints[i]) {
+          setCurrentBreakpoint('sm')
+          break
+        } else if (
+          width > sortedBreakpoints[i - 1] &&
+          width <= sortedBreakpoints[i]
+        ) {
+          setCurrentBreakpoint('md')
+          break
+        } else if (
+          i + 1 === sortedBreakpoints.length &&
+          width > sortedBreakpoints[i]
+        ) {
+          setCurrentBreakpoint('lg')
+          break
+        }
+      }
+    },
+    [BPS],
+  )
+
   useEffect(() => {
     if (!containerEl) return
 
     const callback = (entries: ResizeObserverEntry[]) => {
-      console.log(entries)
+      const [entry] = entries
+      const { width, height } = entry.contentRect
+      determineCurrentBreakpoint({
+        width,
+        height,
+      })
     }
 
-    const resizeObserver = new ResizeObserver(_debounce(callback))
+    const resizeObserver = new ResizeObserver(callback)
 
     resizeObserver.observe(containerEl)
 
     return () => {
       resizeObserver.disconnect()
     }
-  }, [containerEl])
+  }, [containerEl, determineCurrentBreakpoint])
 
-  return currentBreakpoint
+  return {
+    currentBreakpoint,
+  }
 }
