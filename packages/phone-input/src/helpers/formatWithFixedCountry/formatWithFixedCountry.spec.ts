f967bbd4d0ceb989d7e10a13606cb8a11b5d6476
Thinh Trinh <thinh.duc.trinh@gmail.com>
2024-03-06 12:28:51 +0700

    chore(usephoneinput): increased use phone input coverage (#86)

--- packages/phone-input/src/helpers/formatWithFixedCountry/formatWithFixedCountry.spec.ts
+++ packages/phone-input/src/helpers/formatWithFixedCountry/formatWithFixedCountry.spec.ts
@@ -1,11 +1,17 @@
 import { formatWithFixedCountry } from './formatWithFixedCountry'
 
 describe('formatWithFixedCountry', () => {
+  it('Should return empty string when phone value is invalid', () => {
+    expect(formatWithFixedCountry('', 'US')).toBe('')
+  })
+
   it('Should leave as-is when the value is already has valid country code.', () => {
     expect(formatWithFixedCountry('+123456', 'US')).toBe('+123456')
   })
 
   it('Should format to the correct country code.', () => {
     expect(formatWithFixedCountry('+123456', 'VN')).toBe('+84123456')
+    expect(formatWithFixedCountry('0123456', 'VN')).toBe('+84123456')
+    expect(formatWithFixedCountry('123456', 'VN')).toBe('+84123456')
   })
 })
